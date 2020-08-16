6b2de5279f32d51f932209a57410fd48efbd6f0b
Yang Sun <yangsun@didymium-2.cmpgroup.ameslab.gov>
2020-08-16 01:58:01 -0500

    recp direct->cartesian

--- phonopy2matdyn.py
+++ phonopy2matdyn.py
@@ -2,6 +2,7 @@
 
 import yaml
 import sys
+import numpy as np
 
 THz2cm=33.35641 # 1THz -> cm^-1
 
@@ -18,6 +19,25 @@ for i in range(natom):
     print(atoms[i]['symbol'],atoms[i]['mass'])
     mass.append(atoms[i]['mass'])
 
+latt=data.get('lattice')
+latt=np.array(latt)
+alat=np.sqrt(np.dot(latt[0],latt[0]))
+latt=latt/alat
+print("lattice:")
+print(latt)
+
+c12=np.cross(latt[1],latt[2])
+c20=np.cross(latt[2],latt[0])
+c01=np.cross(latt[0],latt[1])
+
+b0=c12 / np.dot(latt[0],c12)
+b1=c20 / np.dot(latt[1],c20)
+b2=c01 / np.dot(latt[2],c01)
+
+recp=np.array([b0,b1,b2])
+print("reciprocal lattice:")
+print(recp)
+
 # phonons
 nbnd=natom*3
 ph=data.get('phonon')
@@ -28,7 +48,10 @@ print("&plot nbnd=   "+str(nbnd)+", nks=  "+str(num_ph)+" /",file=fout)
 fw=open("weight","w+")
 print("weight",file=fw)
 for i in range(num_ph):
-    Qp=ph[i]['q-position']
+    #Qp=np.array(ph[i]['q-position'])
+    qp_dirt=np.array(ph[i]['q-position'])
+    qp_cart=np.dot(qp_dirt,recp)
+    Qp=qp_cart
     weight=ph[i]['weight']
     freq=[]
     for k in range(nbnd):
