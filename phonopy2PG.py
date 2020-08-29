f4f00caf08e6393ce8406e923714ded5d94f3046
Yang Sun <yangsun@didymium-2.cmpgroup.ameslab.gov>
2020-08-29 00:05:08 -0500

    phonopy2PG.py fix

--- phonopy2PG.py
+++ phonopy2PG.py
@@ -27,21 +27,24 @@ def pv_BM(params, vol):
     p=3*B0/2*(eta**7-eta**5)*(1+3/4*(Bp-4)*(eta**2-1))*160.21765 # GPa
     return p
 
-def fit_BM(eos,name,temp):
+def get_BM(eos,name,temp):
     birch_murn = eos
     # compute PH
-    pfit = np.linspace(float(args.press[0]),float(args.press[1]), 101)
+    Pmin=float(args.press[0]); Pmax=float(args.press[1])
+    Pn=int(Pmax-Pmin+0.5)+1
+    pfit = np.linspace(Pmin,Pmax,Pn)
     vfit = []
     for Pi in pfit:
         func = lambda V : pv_BM(birch_murn,V)-Pi
-        vok  = fsolve(func,x0=vol.min())
+        vok  = fsolve(func,x0=2.0)
         vfit.append(vok[0])
+    vfit=np.array(vfit)
     out_P=pv_BM(birch_murn,vfit)
     out_G=eos_birch_murnaghan(birch_murn,vfit)+pv_BM(birch_murn,vfit)*vfit/160.21765
     fout=open(name,"w+")
-    print("#T=",temp,file=fout)
+    print("#P(GPa) G(eV/cell)  #T=",temp,"K",file=fout)
     for k in range(len(out_P)):
-        print(out_P[k],out_G[k],file=fout)
+        print("%.6f  %.10f"%(out_P[k],out_G[k]),file=fout)
     fout.close()
 
 temp=args.temperatures
@@ -52,8 +55,6 @@ for line in fin:
         ll=line.split()
         tt=ll[2].split(".")[0]
         if(tt in temp):
-            print(tt,end=" ")
-#            print(fin.readline().strip("\n"))
-            ll=line.split() # E0 B0 Bp V0 of helmholtz
-            eos=[float(ll[k]) for k in range(2,6)]
+            lB=fin.readline().split() # E0 B0 Bp V0 of helmholtz
+            eos=[float(lB[k]) for k in range(2,6)]
             get_BM(eos,"PG_"+tt+"K.dat",tt)
