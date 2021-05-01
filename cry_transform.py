6eb71b382f5e81be9be86f49ddcc4a85afdadd39
Yang Sun <yangsun@didymium-2.cmpgroup.ameslab.gov>
2021-05-01 15:14:25 -0500

    cry_transform

---
+++ cry_transform.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import ase.build
+import ase.io.vasp
+import sys
+cell = ase.io.vasp.read_vasp(sys.argv[1])
+#P = [[9,0,0],[6,12,0],[0,0,3]]
+P = [[1,1,0],[1,1,0],[0,0,2]] # HCP -> SC
+sc = ase.build.make_supercell(cell, P)
+ase.io.vasp.write_vasp("POSCAR.sc",sc, label='supercell',direct=True,sort=True)
