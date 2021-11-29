1133e9b255466ad03e3805a4f61bec3f17dce08d
Yang <yangsun1017@gmail.com>
2021-11-29 00:36:02 -0600

    range

--- ana-lmd-plot.py
+++ ana-lmd-plot.py
@@ -27,7 +27,7 @@ plt.ylabel("$\omega$ (meV)")
 
 plt.subplot(3,1,2)
 plt.plot(data[:-3,0], lmd_star[:-3],"-o", color="k")
-plt.xlim(-0.5, data[-1][0]+0.5)
+plt.xlim(-0.5, max(data[:,0]+0.5))
 plt.ylim(-0.05, max(1.05,max(lmd_star[:-3])))
 plt.ylabel("$\lambda^{*}$")
 
