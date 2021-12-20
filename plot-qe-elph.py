44690826954dbfa31558f9bafe78f3c226e4678d
Yang Sun <yangsun@didymium-2.cmpgroup.ameslab.gov>
2021-12-19 19:41:55 -0600

    add plot qe elph

---
+++ plot-qe-elph.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import glob
+import numpy as np
+import pylab as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d","--disp", help="dispersions", action='store')
+parser.add_argument("-g","--gamma", help="elph.gamma file", action='store')
+parser.add_argument("-a","--a2f", help="a2f.dos file", action='store')
+parser.add_argument("-k","--kp", help="k points", nargs='*',action='store')
+parser.add_argument("-n","--number", help="number of spacing",action='store')
+args = parser.parse_args()
+
+# disperion parameters
+#nspacing=50; kpoints=['G','M','K','G','A','M','H']
+nspacing=int(args.number); kpoints=args.kp
+comment='tst'
+# files
+fls=args.disp  #"band.gp" # dispersion file
+ngauss=10
+gamma_fls=args.gamma #"elph.gamma.3"
+a2F_fls=args.a2f #"a2F.dos2"
+#gamma_fls=["elph.gamma."+str(k) for k in range(1,1+ngauss)] # gamma files
+#a2F_fls=["a2F.dos"+str(k) for k in range(1,1+ngauss)]  # a^2F(w)
+
+plt.rcParams.update({'font.size': 14})
+f, (a0, a1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [2, 1]}, figsize=(12,6))
+
+cm2Thz=0.02998
+
+# read gamma file
+fin=open(gamma_fls,"r")
+ll=fin.readline().split()
+nbnd=int(ll[2].strip(",")) 
+nks=int(ll[-2])
+gdata=[]
+nl=int((nbnd-1)/6)+1
+for k in range(nks):
+    fin.readline()
+    ng=[]
+    for j in range(nl):
+        ll=fin.readline().split()
+        for il in ll:
+            ng.append(float(il))
+    gdata.append(ng)
+fin.close()
+gdata=np.array(gdata)
+
+# read dispersion
+ct=0
+data=np.loadtxt(fls)
+
+#print(gdata.shape)
+#print(data.shape)
+
+for k in range(1,data.shape[1]):
+    if(k==1):
+        a0.plot(data[:,0],data[:,k]*cm2Thz,linestyle='-',color='k',label=comment)
+    else:
+        a0.plot(data[:,0],data[:,k]*cm2Thz,linestyle='-',color='k')
+    a0.scatter(data[:,0],data[:,k]*cm2Thz,s=abs(gdata[:,k-1])*.1,color='r')
+ct+=1
+
+
+ndata=data.shape[0]
+nv=int((ndata-1)/nspacing+0.5)
+vx=[data[k*nspacing][0] for k in range(1,nv)]
+for iv in vx:
+    a0.axvline(iv,color='k',lw=0.5)
+a0.axhline(0,color='k',lw=0.5)
+
+a0.set_xlim(data[0][0],data[-1][0])
+ymax=np.amax(data[:,1:])
+ymin=np.amin(data[:,1:])
+a0.set_ylim(ymin*cm2Thz-5,ymax*cm2Thz+5)
+
+a=[data[0][0]]; b=[data[-1][0]]
+xlb=a+vx+b
+a0.set_xticks(xlb)
+a0.set_xticklabels(kpoints)
+a0.set_ylabel(r"Frequency (THz)")
+#a0.legend()
+
+# read a2F
+fin=open(a2F_fls,"r")
+freq=[]; a2F=[]
+for k in range(5):
+    fin.readline()
+lmd=-1
+for line in fin:
+    if("lambda" in line):
+        lmd=float(line.split()[2])
+        break
+    ll=line.split()
+    freq.append(float(ll[0]))
+    a2F.append(float(ll[1]))
+fin.close()
+
+Ry2Thz=3289.8449
+a1.plot(a2F, np.array(freq)*Ry2Thz,label=r"$\lambda=$%.2f"%(lmd))
+a1.set_ylim(ymin*cm2Thz-5,ymax*cm2Thz+5)
+a1.set_xlabel(r"$\alpha^2F(\omega)$")
+a1.legend()
+
+f.tight_layout()
+f.savefig("elph.png")
