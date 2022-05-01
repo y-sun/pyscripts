dfe13e04428cc3cdb50153fd8dead9439b3cef7b
Yang Sun <ys3339@br012.ib.bridges2.psc.edu>
2022-05-01 17:15:10 -0400

    add plot-vaspkit-band

--- plot-vaspkit-pdos.py
+++ plot-vaspkit-pdos.py
@@ -8,6 +8,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-f","--files", help="PDOS files", nargs='*',action='store')
 parser.add_argument("-t","--title", help="plot title",action='store')
+parser.add_argument("-e","--energy", help="fermi energy (0 for vaspkit)",action='store')
 parser.add_argument("-x","--xlim", help="xrange", nargs='*',action='store')
 parser.add_argument("-y","--ylim", help="yrange", nargs='*',action='store')
 
@@ -16,10 +17,15 @@ args = parser.parse_args()
 plt.figure(figsize=(8,6))
 plt.rcParams.update({'font.size': 16})
 
+if (args.energy is not None):
+    Ef=float(args.energy)
+else:
+    Ef=0
+
 nfile=len(args.files)
 for i in range(nfile):
     data=np.loadtxt(args.files[i],skiprows=1)
-    plt.plot(data[:,0],data[:,-1],label=args.files[i].split('.')[0])
+    plt.plot(data[:,0]-Ef,data[:,-1],label=args.files[i].split('.')[0])
 plt.axvline(0.0,ls='--',c='k')
 plt.legend(loc='upper right',fontsize=14)
 
