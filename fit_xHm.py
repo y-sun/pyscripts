5880b17d080bf461ff38c0fd1b43982239bbd06d
Yang <yangsun1017@gmail.com>
2022-05-14 16:17:56 -0500

    add 2nd approx.

--- fit_xHm.py
+++ fit_xHm.py
@@ -16,17 +16,28 @@ def Hm_model(para,x):
    f=b*x-b*x*x
    return f
 
-data=np.loadtxt('xH.dat',skiprows=1)
+
+def Hm_model_2nd(para,x):
+   b,z=para
+   f=b*x-b*x*x-2*b**2*x**2*(1-x)**2/z
+   return f
+
+data=np.loadtxt(args.input,skiprows=1)
 c=data[:,0]
 H=data[:,-1]
 H_A=data[0,-1]
 H_B=data[-1,-1]
 Hm=H - (1-c)*H_A - c * H_B
 
-# least sq fit
+# least sq fit 
 target =  lambda para, y, x : y - Hm_model(para,x)
 x0= 0.5
-fitted, err = leastsq(target, x0, args=(Hm, c))
+fitted1, err = leastsq(target, x0, args=(Hm, c))
+
+# least sq fit for 2nd Hm model
+target =  lambda para, y, x : y - Hm_model_2nd(para,x)
+x0 = [fitted1[0],1.0]
+fitted2, err = leastsq(target, x0, args=(Hm, c))
 
 # plot
 import pylab as plt
@@ -35,7 +46,8 @@ plt.rcParams.update({'font.size': 16})
 
 craw = np.linspace(0,1,100)
 plt.plot(c, Hm, 'ro', label='raw')
-plt.plot(craw, Hm_model(fitted[0],craw), label='$H_m=bc(1-c)$, b=%.2f'%(fitted[0]))
+plt.plot(craw, Hm_model(fitted1[0],craw), c='b',label='$H_m=bc(1-c)$, b=%.2f'%(fitted1[0]))
+plt.plot(craw, Hm_model_2nd(fitted2,craw), c='g',label='$H_m=bc(1-c)-2[bc(1-c)]^2/z$\nb=%.2f, z=%.2f'%(fitted2[0],fitted2[1]))
 plt.xlabel('$c$')
 plt.ylabel('$H_m$ (eV/atom)')
 plt.legend()
@@ -47,4 +59,5 @@ plt.savefig('xH.png')
 if (args.show):
    plt.show()
 
-print('b=',fitted[0])
+print('b=',fitted1[0])
+print('b,z=',fitted2[0],fitted2[1])
