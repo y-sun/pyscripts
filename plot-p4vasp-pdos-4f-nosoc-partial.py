43322a4cc5aafa273372a95cfba6e47a9ce410d5
Yang <yangsun1017@gmail.com>
2022-08-07 15:09:54 -0500

    -1 for down

--- plot-p4vasp-pdos-4f-nosoc-partial.py
+++ plot-p4vasp-pdos-4f-nosoc-partial.py
@@ -6,11 +6,14 @@ data=calc.dos.read(selection="1(fy3x2) 1(fxyz) 1(fyz2) 1(fz3) 1(fxz2) 1(fzx2) 1(
 ldata=list(data)
 
 fout=open("PDOS_f_angular.dat","w+")
+tag=[1 for k in range(17)] # 17 = 1 + 2 + 7*2
 for k in range(17):
     print(ldata[k],end=" ",file=fout)
+    if("down" in ldata[k]):
+        tag[k]=-1
 print("", file=fout)
 for k in range(data['energies'].size):
     for i in range(17):
-        print(data[ ldata[i] ][k], end=" ", file=fout)
+        print(tag[k]*data[ ldata[i] ][k], end=" ", file=fout)
     print("",file=fout)
 fout.close()
