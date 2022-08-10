b3a3be8ee2227b6cc46fc67c165a1afcb97c39c1
Yang Sun <ys3339@br011.ib.bridges2.psc.edu>
2022-08-10 13:04:36 -0400

    add plot

--- plot-p4vasp-pdos-4f-nosoc-partial.py
+++ plot-p4vasp-pdos-4f-nosoc-partial.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+import argparse
 from py4vasp import Calculation
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p","--plot", help="plot histogram", action='store_true')
+args = parser.parse_args()
+
 calc = Calculation.from_path(".")
 data=calc.dos.read(selection="1(fy3x2) 1(fxyz) 1(fyz2) 1(fz3) 1(fxz2) 1(fzx2) 1(fx3)")
 ldata=list(data)
@@ -17,3 +23,32 @@ for k in range(data['energies'].size):
         print(tag[i]*data[ ldata[i] ][k], end=" ", file=fout)
     print("",file=fout)
 fout.close()
+
+if (args.plot):
+   import pylab as plt
+   import numpy as np
+   plt.figure(figsize=(8,6))
+   plt.rcParams.update({'font.size': 15})
+   
+   fin=open("PDOS_f_angular.dat","r")
+   name=fin.readline().split()
+   fin.close()
+   
+   col=['r','c','g','y','m','olive','b']
+   
+   data=np.loadtxt("PDOS_f_angular.dat",skiprows=1)
+   for k in range(7):
+       if(k>4): lsp='dashed'
+       else:   lsp='-'
+       lb=name[k*2+3].split('_')[2]
+       plt.plot(data[:,0], data[:,k*2+3], c=col[k], ls=lsp,label=lb)
+       plt.plot(data[:,0], data[:,k*2+4], c=col[k], ls=lsp)
+   
+   plt.axvline(0,ls='dotted',c='k')
+   plt.xlim(-2,6)
+   #plt.ylim(-10,10)
+   plt.xlabel('E-E$_f$ (eV/atom)')
+   plt.ylabel('PDOS')
+   plt.legend()
+   plt.tight_layout()
+   plt.savefig('fig.png')
