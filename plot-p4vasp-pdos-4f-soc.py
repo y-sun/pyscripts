459e849461e7f7172ff923773fe1e3f22c6340dd
Yang Sun <ys3339@br012.ib.bridges2.psc.edu>
2022-08-21 14:43:09 -0400

    soc

---
+++ plot-p4vasp-pdos-4f-soc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import argparse
+import numpy as np
+import pylab as plt
+from py4vasp import Calculation
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o","--orbitals", help="orbital, e.g. 1(d) 2(f)", action='store')
+parser.add_argument("-t","--title", help="plot title",action='store')
+parser.add_argument("-x","--xlim", help="xrange", nargs='*',action='store')
+parser.add_argument("-y","--ylim", help="yrange", nargs='*',action='store')
+args = parser.parse_args()
+
+plt.figure(figsize=(8,6))
+plt.rcParams.update({'font.size': 16})
+
+calc = Calculation.from_path(".")
+data = calc.dos.read(selection=args.orbitals)
+
+d=data[ list(data)[2] ] 
+f=data[ list(data)[3] ]
+
+fout=open("PDOS_df.dat",'w+')
+print("#E-Ef dos_d dos_f",file=fout)
+for k in range(data['energies'].size):
+    print(data['energies'][k],d[k],f[k],file=fout)
+fout.close()
+
+plt.plot(data['energies'],d,c='g',label=list(data)[2])
+plt.plot(data['energies'],f,c='r',label=list(data)[3])
+plt.axvline(0.0,ls='--',c='k')
+
+if(args.xlim is not None):
+    plt.xlim(float(args.xlim[0]), float(args.xlim[1]))
+else:
+    plt.xlim(-5,5)
+
+if(args.ylim is not None):
+    plt.ylim(float(args.ylim[0]), float(args.ylim[1]))
+
+plt.xlabel('$E-E_f$ (eV)')
+plt.ylabel('PDOS (states/eV/atom)')
+plt.title(args.title)
+plt.tight_layout()
+plt.savefig("dos.png")
+#plt.show()
