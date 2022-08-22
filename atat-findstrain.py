750bf4c376be34cb9cd24bb2ca7227c42ae5a9f3
Yang Sun <ys3339@br012.ib.bridges2.psc.edu>
2022-08-22 16:24:15 -0400

    add 0.05 cut

--- atat-findstrain.py
+++ atat-findstrain.py
@@ -8,3 +8,15 @@ for line in fin:
     if(float(ll[0]) > scut):
         print(ll[1],end=" ")
 print("")
+fin.close()
+
+
+scut=0.05
+print("strain larger than 0.05:")
+fin=open("checkrelax.out",'r')
+for line in fin:
+    ll=line.split()
+    if(float(ll[0]) > scut):
+        print(ll[1],end=" ")
+print("")
+fin.close()
