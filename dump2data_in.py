f117e774b05cb3a97e061b5920a39fa5d3f9479a
Yang Sun <ys3339@login02.expanse.sdsc.edu>
2022-09-13 08:23:52 -0700

    add O mass

--- dump2data_in.py
+++ dump2data_in.py
@@ -8,7 +8,7 @@ mass=[]
 filename=sys.argv[1]
 step=sys.argv[2]
 leng=len(sys.argv)
-dic={'Tb': 158.92534,'Co':58.933,'Zr':91.224,'Hf':178.490,'Al':26.983,'Fe':55.845,
+dic={'Tb': 158.92534,'Co':58.933,'Zr':91.224,'Hf':178.490,'Al':26.983,'Fe':55.845,'O':15.999,
      'Sm':150.360,'Cu':63.546,'Ni':58.71, 'Mg':24.305, 'P': 30.974, 'Si': 28.0855 , 'Sn': 118.71 }
 for i in range(1,leng-2): 
    mass.append(dic[sys.argv[2+i]])
